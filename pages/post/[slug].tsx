7aecdae4c72e03d9d06d48d7e71e32a6d4e5aa50
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2022-07-23 08:59:20 +0900

    Add everythin

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { getPosts, getPostDetails } from '../../services';
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentForm } from '../../components';
+function PostDetails() {
+    return (
+        // https://youtu.be/HYv55DhgTuA?t=5894
+            <div></div>
+    );
+}
+
+export default PostDetails;
